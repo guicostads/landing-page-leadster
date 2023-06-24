1ec32c04f713dafbbfdb3d13d38921aa62dd13c6
Guilherme Costa <guicost98@gmail.com>
2023-06-24 13:13:34 -0300

    feat: header component

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import TopImage from './images/logo.png'
+import { Header } from '@/components/Header';
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-col items-center p-6">
+        <Image src={TopImage} width={150} height={300} alt='leadster logo' />
+      </div>
+      <Header />
+    </main>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+
+
+export const Header = () => {
+  return <div className="w-full bg-sky-500/[.09] p-5 h-80 flex items-center justify-center">
+   <h1>oi</h1>
+  </div>;
+};
