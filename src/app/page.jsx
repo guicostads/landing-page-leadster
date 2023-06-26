332a13dc78e3d7e53feef9e8e6538a2a7176f87b
Guilherme Costa <guicost98@gmail.com>
2023-06-26 20:10:55 -0300

    feat: header component css changes #1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import { Header } from '@/components/Header';
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center p-6">
+      <div className="flex flex-col items-center justify-center	p-6">
         <Image src={TopImage} width={150} height={300} alt='leadster logo' />
       </div>
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-
-
 export const Header = () => {
-  return <div className="w-full bg-sky-500/[.09] p-5 h-80 flex items-center justify-center">
-   <h1>oi</h1>
-  </div>;
+  return (
+    <div className="w-full bg-sky-500/[.08] p-5 h-80 flex flex-col items-center justify-center">
+      <div className="w-50 bg-transparent border-solid border-2 p-0.5 border-blue-500 rounded-full rounded-bl-none text-blue-500 mb-3 uppercase text-xs whitespace-nowrap px-3">
+        Webinars exclusivos
+      </div>
+      <span className="text-gray-800 text-4xl">Menos conversinha,</span>
+      <div className="border-b border-solid border-b-gray-300/[.55] text-blue-500 text-7xl font-semibold">
+        Mais conversão
+      </div>
+      <p className="p-1">Conheça as estratégias que <span>mudaram o jogo</span> e como aplicá-las ao seu negócio.</p>
+    </div>
+  );
 };
