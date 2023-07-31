654c4bda432e9a94d62af375ddf49be8d8653c4c
Guilherme Costa <guicost98@gmail.com>
2023-07-31 14:56:58 -0300

    feat: layout/css changes

--- src/app/components/BottomContent.jsx
+++ src/app/components/BottomContent.jsx
@@ -4,9 +4,11 @@ import comparativo from "@/app/images/comparativo_img_CTA.png";
 const BottomContent = () => {
   return (
     <div className="bg-sky-500/[.08] w-full flex items-center justify-center gap-4">
-      <div className="grid grid-cols-2 place-items-center">
-        <Image src={comparativo} alt="comparativo" className="w-96 h-auto" />
-        <div>oi</div>
+      <div className="flex justify-center gap-3 items-center max-md:px-4">
+        <div className='w-3/4'>
+          <Image src={comparativo} alt="comparativo" />
+        </div>
+        <div className="w-1/2">oi</div>
       </div>
     </div>
   );
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import Logo from "@/app/images/logo.png";
 
-
-
 const Footer = () => {
   return (
     <footer className="flex flex-col justify-center pt-4 w-full">
-      <Image src={Logo} alt="logo" />
-      <p className="text-gray-500 text-sm">
-        Transformando visitantes em clientes.
-      </p>
-      <div className="grid grid-cols-4 max-md:grid-cols-1 max-md:px-5 place-items-center max-md:place-items-stretch py-3 px-20 mt-2">
-        <div className="text-left">
-          <h2 className="mb-7 text-gray-700 font-medium">Links Principais</h2>
+      <div className="flex flex-col m-auto">
+        <Image src={Logo} alt="logo" />
+        <p className="text-gray-500 text-sm m-auto">
+          Transformando visitantes em clientes.
+        </p>
+      </div>
+      <div className="grid grid-cols-4 max-md:grid-cols-1 max-md:px-5 place-items-center py-3 px-20 mt-2">
+        <div className="text-left max-md:text-center">
+          <h2 className="mb-4 text-gray-700 font-medium">Links Principais</h2>
           <ul className="text-gray-400 flex flex-col gap-2">
             <li className="cursor-pointer">Home</li>
             <li className="cursor-pointer">Ferramenta</li>
@@ -20,8 +20,8 @@ const Footer = () => {
             <li className="cursor-pointer">Contato</li>
           </ul>
         </div>
-        <div className="text-left">
-          <h2 className="mb-7 text-gray-700 font-medium">Cases</h2>
+        <div className="text-left max-md:text-center">
+          <h2 className="mb-4 mt-4 text-gray-700 font-medium">Cases</h2>
           <ul className="text-gray-400 flex flex-col gap-2">
             <li className="cursor-pointer">Geração de leads B2B</li>
             <li className="cursor-pointer">Geração de leads</li>
@@ -29,8 +29,8 @@ const Footer = () => {
             <li className="cursor-pointer">Cases de sucesso</li>
           </ul>
         </div>
-        <div className="text-left">
-          <h2 className="mb-7 text-gray-700 font-medium">Materiais</h2>
+        <div className="text-left max-md:text-center">
+          <h2 className="mb-4 mt-4 text-gray-700 font-medium">Materiais</h2>
           <ul className="text-gray-400 flex flex-col gap-2">
             <li className="cursor-pointer">Blog</li>
             <li className="cursor-pointer">Parceria com agências</li>
@@ -39,10 +39,8 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h2 className="mb-7 text-gray-700 font-medium">Siga a leadster</h2>
-         <div className="flex gap-3">
-          
-         </div>
+          <h2 className="mb-4 mt-4 text-gray-700 font-medium">Siga a leadster</h2>
+          <div className="flex gap-3"></div>
         </div>
       </div>
     </footer>
